cd57ada8ab6cc09410925e2bcd65b5a99fb8389f
pmw57 <paul.m.wilkins@gmail.com>
2025-12-19 03:58:28 +1300

    Fix: css styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,12 @@
 export default {
   content: [
     "./index.html",
-    "./App.tsx",
-    "./index.tsx",
-    "./components/**/*.tsx",
+    "./*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./data/**/*.{js,ts,jsx,tsx}",
+    "./hooks/**/*.{js,ts,jsx,tsx}",
+    "./state/**/*.{js,ts,jsx,tsx}",
+    "./utils/**/*.{js,ts,jsx,tsx}",
   ],
   darkMode: 'class',
   theme: {
