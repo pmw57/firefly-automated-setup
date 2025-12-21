44a7f95d6fbb1ee461b85a2e75bfd647b955a75f
pmw57 <paul.m.wilkins@gmail.com>
2025-12-22 02:28:07 +1300

    SEO improvements

--- vite.config.ts
+++ vite.config.ts
@@ -55,7 +55,7 @@ export default defineConfig({
     tailwindCdnFallbackPlugin(), // Add our custom plugin
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['expansion_sprites.png', 'logo.svg'],
+      includeAssets: ['expansion_sprites.png', 'logo.svg', 'robots.txt', 'sitemap.xml', 'google45bc800396599bee.html'],
       manifest: {
         name: 'Firefly Automated Setup Guide',
         short_name: 'Firefly Setup',
