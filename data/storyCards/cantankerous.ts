664d850623a164f14c1c60b1281dc4ad07b04d30
pmw57 <paul.m.wilkins@gmail.com>
2025-12-27 11:29:54 +1300

    Add story cards for ship pack wave one

--- data/storyCards/cantankerous.ts
+++ data/storyCards/cantankerous.ts
@@ -1,4 +1,11 @@
-
 import { StoryCardDef } from '../../types/index';
 
-export const CANTANKEROUS_STORIES: StoryCardDef[] = [];
+export const CANTANKEROUS_STORIES: StoryCardDef[] = [
+    {
+        title: "Reap The Whirlwind",
+        intro: "Word is, the Alliance has been hiding all manner of dirty secrets out Himinbjorg way. Convince the Dust Devils you're dangerous -- or desperate -- enough for them to come out of hiding and join forces. But hurry: the upcoming Unification Day Summit seems like the perfect time to let the truth out.",
+        sourceUrl: "https://boardgamegeek.com/image/7072594/gwek",
+        requiredExpansion: "cantankerous",
+        additionalRequirements: ["blue", "kalidasa"],
+    },
+];
