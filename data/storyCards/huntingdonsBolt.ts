a7dc8ad42927249774b1bb54b99349e501a63b13
pmw57 <paul.m.wilkins@gmail.com>
2026-01-03 10:37:55 +1300

    Update: Under the Radar story card

--- data/storyCards/huntingdonsBolt.ts
+++ data/storyCards/huntingdonsBolt.ts
@@ -3,20 +3,26 @@ import { createStoryRules } from './utils';
 
 export const HUNTINGDONS_BOLT_STORIES: StoryCardDef[] = [
     {
-        "title": "Under The Radar",
-        "intro": "There's a client who'll pay good coin if you can crack the protected ICE server at Boros... but the usual suspects may draw to much attention. Best keep the crew small.",
-        "sourceUrl": "https://boardgamegeek.com/image/8455024/gwek",
-        "requiredExpansion": "huntingdons_bolt",
-        "additionalRequirements": ["blue"],
-        "rules": createStoryRules("Under The Radar", [
+        title: "Under The Radar",
+        intro: "There's a client who'll pay good coin if you can crack the protected ICE server at Boros... but the usual suspects may draw to much attention. Best keep the crew small.",
+        sourceUrl: "https://boardgamegeek.com/thread/3281169/article/47098185#47098185",
+        requiredExpansion: "huntingdons_bolt",
+        additionalRequirements: [
+            "blue"
+        ],
+        rules: [
             {
-                type: 'addSpecialRule',
-                category: 'goal',
+                type: "addSpecialRule",
+                category: "goal",
                 rule: {
                     title: "Discretion Required",
-                    content: [ "When you Work a Goal, you must first proceed past a Misbehave for each Max Crew over 4." ]
-                }
+                    content: [
+                        "When you Work a Goal, you must first proceed past a Misbehave for each Max Crew over 4."
+                    ]
+                },
+                source: "story",
+                sourceName: "Under The Radar"
             }
-        ]),
+        ]
     }
 ];
