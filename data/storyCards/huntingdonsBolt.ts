852fcb7a7561bb65a8ab49cc6c7418073fc59c0a
pmw57 <paul.m.wilkins@gmail.com>
2026-01-07 15:51:29 +1300

    Fix: Under The Radar, Story override not needed

--- data/storyCards/huntingdonsBolt.ts
+++ data/storyCards/huntingdonsBolt.ts
@@ -9,19 +9,5 @@ export const HUNTINGDONS_BOLT_STORIES: StoryCardDef[] = [
         additionalRequirements: [
             "blue"
         ],
-        rules: [
-            {
-                type: "addSpecialRule",
-                category: "goal",
-                rule: {
-                    title: "Discretion Required",
-                    content: [
-                        "When you Work a Goal, you must first proceed past a Misbehave for each Max Crew over 4."
-                    ]
-                },
-                source: "story",
-                sourceName: "Under The Radar"
-            }
-        ]
     }
 ];
